7cfb41b0d46bf26808c99d59a3c1d47252eda44b
Kulgann <yuriy@incineration.games>
2021-10-01 14:10:19 +0300

    Fixed up package structure

---
+++ Editor/CustomColorPropDef.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "CustomColorPropDef", menuName = "ScriptableObjects/Custom Color properties", order = 2)]
+public class CustomColorPropDef : ScriptableObject
+{
+    public List<string> CustomColorPropDefines;
+}
